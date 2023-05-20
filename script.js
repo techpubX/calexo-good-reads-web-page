741acd5ce115b5ab8251ccec54a5531db087a068
OYOOCALEX2022 <calexoyoo400@gmail.com>
2023-05-20 16:28:23 +0300

    added scripts

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+var typed = new Typed(".multiple-text", {
+  strings: ["Discover", " Your Next", "Favourite Book"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 100,
+  loop: true,
+});
 document.onload = () => {
 const navLinks = document.querySelectorAll(".nav_link");
 
