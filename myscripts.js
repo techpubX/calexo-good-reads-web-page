a9a68ce4edea9b329d34cf2918d17c1028106981
OYOOCALEX2022 <calexoyoo400@gmail.com>
2023-09-23 23:57:27 +0300

    added js content

--- myscripts.js
+++ myscripts.js
@@ -7,4 +7,12 @@
      navList.classList.toggle("show");
  });
 
-
+ document.addEventListener("DOMContentLoaded", function () {
+  const seeMoreButton = document.getElementById("see-more-button");
+  const fullDescription = document.querySelector(".full-description");
+  
+  seeMoreButton.addEventListener("click", function () {
+    fullDescription.style.display = "block";
+    seeMoreButton.style.display = "none";
+  });
+});
