5adeab14524dc34a5d1e925a8e46c1a5943f4aff
OYOOCALEX2022 <calexoyoo400@gmail.com>
2023-09-23 13:43:25 +0300

    edited css for search bar

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
  // JavaScript for responsive navigation
- 
+
  const navToggle = document.getElementById("nav-toggle");
  const navList = document.querySelector(".nav__list");
 
@@ -8,24 +8,3 @@
  });
 
 
-document.onload = () => {
-const navLinks = document.querySelectorAll(".nav_link");
-
-navLinks.forEach((link) => {
-  link.addEventListener("click", (event) => {
-    event.preventDefault(); 
-
-    const id = link.getAttribute("href");
-    console.log(id);
-
-    const targetElement = document.querySelector(id);
-    console.log(targetElement);
-
-    if (targetElement) {
-      targetElement.scrollIntoView({
-        behavior: "smooth",
-      });
-    }
-  });
-});
-}
