39ce23d55bf91a2e8b5fe009940138431ff504b0
OYOOCALEX2022 <calexoyoo400@gmail.com>
2023-09-25 19:43:05 +0300

    added nav link js code

--- myscripts.js
+++ myscripts.js
@@ -12,3 +12,25 @@
     }
   });
 });
+
+document.onload = () => {
+  const navLinks = document.querySelectorAll(".nav_link");
+  
+  navLinks.forEach((link) => {
+    link.addEventListener("click", (event) => {
+      event.preventDefault(); 
+  
+      const id = link.getAttribute("href");
+      console.log(id);
+  
+      const targetElement = document.querySelector(id);
+      console.log(targetElement);
+  
+      if (targetElement) {
+        targetElement.scrollIntoView({
+          behavior: "smooth",
+        });
+      }
+    });
+  });
+  }
